2f6d3e4a243b20638baa0821f8ee208f8758502a
LAPTOP-N6HJER3M\mengchu <mengchu35@gmail.com>
2021-11-12 10:32:40 +0800

    [vue] v-for

--- src/components/BicycleRentalShop.vue
+++ src/components/BicycleRentalShop.vue
@@ -6,6 +6,10 @@
     <button v-on:click="minus" v-bind:disabled="btnMinus()">-1</button>
     <p v-if="count == 0">No bike is available</p>
     <p v-else>Bikes are available</p>
+    <p>Rules</p>
+    <ol>
+      <li v-for="(rule, index) in rules" v-bind:key="index">{{ rule }}</li>
+    </ol>
     <!--<p>Total income: {{ subtotalComputed }}</p>-->
   </div>
 </template>
@@ -18,6 +22,11 @@ export default {
       price: 100,
       quantity: 20, // 總量
       count: 20, // 剩餘個數
+      rules: [
+        '$100 each time.',
+        'Please return no later than 18:00.',
+        'Be safe.'
+      ],
     }
   },
   computed: {
