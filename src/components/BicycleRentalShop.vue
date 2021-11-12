e0cad554fa9a0681c4a15cdf403d7403aa956053
LAPTOP-N6HJER3M\mengchu <mengchu35@gmail.com>
2021-11-12 10:14:53 +0800

    [vue] methods, v-if

--- src/components/BicycleRentalShop.vue
+++ src/components/BicycleRentalShop.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <h1>Bicycle Rental Shop</h1>
-    <p>Number of bicycles: {{ quantity }}</p>
-    <p>Total income: {{ subtotalComputed }}</p>
+    <p>Number of bicycles: {{ count }}</p>
+    <button v-on:click="plus" v-bind:disabled="btnPlus()">+1</button>
+    <button v-on:click="minus" v-bind:disabled="btnMinus()">-1</button>
+    <p v-if="count == 0">No bike is available</p>
+    <p v-else>Bikes are available</p>
+    <!--<p>Total income: {{ subtotalComputed }}</p>-->
   </div>
 </template>
 
@@ -12,13 +16,28 @@ export default {
   data() {
     return {
       price: 100,
-      quantity: 20
+      quantity: 20, // 總量
+      count: 20, // 剩餘個數
     }
   },
   computed: {
     subtotalComputed: function() {
       return this.price * this.quantity;
     }
+  },
+  methods: {
+    plus() {
+      this.count++;
+    },
+    minus() {
+      this.count--;
+    },
+    btnPlus() {
+      return ((this.count >= this.quantity) ? true : false);
+    },
+    btnMinus() {
+      return ((this.count <= 0) ? true : false);
+    },
   }
 }
 </script>
