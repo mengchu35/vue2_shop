204ea3323fe16efb5801361b1bacafe8edfbfdba
LAPTOP-N6HJER3M\mengchu <mengchu35@gmail.com>
2021-11-12 09:22:45 +0800

    [vue] data, computed

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <BicycleRentalShop msg="todoList"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BicycleRentalShop from './components/BicycleRentalShop.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BicycleRentalShop
   }
 }
 </script>
---
+++ src/components/BicycleRentalShop.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Bicycle Rental Shop</h1>
+    <p>Number of bicycles: {{ quantity }}</p>
+    <p>Total income: {{ subtotalComputed }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BicycleRentalShop',
+  data() {
+    return {
+      price: 100,
+      quantity: 20
+    }
+  },
+  computed: {
+    subtotalComputed: function() {
+      return this.price * this.quantity;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
